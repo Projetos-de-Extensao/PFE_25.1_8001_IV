cab4569aeb87b0baf19f71f64e520ef0f4981732
Keanu <keanusantosgermanopereira@gmail.com>
2025-06-16 11:38:53 -0300

    responsividade página de inserir eventos

--- IngressouREACT/src/components/InsertEvent.jsx
+++ IngressouREACT/src/components/InsertEvent.jsx
@@ -27,59 +27,61 @@ function InsertEvent() {
     function handleSubmit(e) {
         e.preventDefault();
     }
+    
     return (
         <>
-            <div className={styles.insertEventContainer}>
-                <div className={styles.header}>
-                    <h1 className={styles.headerText}>
-                        Insira os dados do evento
-                    </h1>
-                </div>
-                <div className={styles.eventInfo}>
-                    <div className={styles.imgInsert}>
-                        <div className={styles.eventImgPreview}>
-                            <img src={imageSrc} alt="preview" className={styles.imgSize}></img>
-                        </div>
-                        <label htmlFor='fileInput' className={styles.buttonStyle}>Selecione uma imagem</label>
-
-                        <input type="file" id="fileInput" accept="image/*" onChange={handleImageChange} style={{ display: 'none' }} className={styles.buttonStyle}></input>
+            <div className={styles.insertEventBody}>
+                <div className={styles.insertEventContainer}>
+                    <div className={styles.header}>
+                        <h1 className={styles.headerText}>
+                            Insira os dados do evento
+                        </h1>
                     </div>
-                    <form className={styles.formStyle}>
-                        <label htmlFor="eventName" className={styles.labelStyle}>Insira o nome do evento</label>
-                        <input type="text" id="eventName" className={styles.inputStyle}></input>
+                    <div className={styles.eventInfo}>
+                        <div className={styles.imgInsert}>
+                            <div className={styles.eventImgPreview}>
+                                <img src={imageSrc} alt="preview" className={styles.imgSize}></img>
+                            </div>
+                            <label htmlFor='fileInput' className={styles.buttonStyle}>Selecione uma imagem</label>
 
-                        <p className={styles.labelStyle}>Insira a descrição do evento</p>
-                        <textarea id="eventDescription"></textarea>
+                            <input type="file" id="fileInput" accept="image/*" onChange={handleImageChange} style={{ display: 'none' }} className={styles.buttonStyle}></input>
+                        </div>
+                        <form className={styles.formStyle}>
+                            <label htmlFor="eventName" className={styles.labelStyle}>Insira o nome do evento</label>
+                            <input type="text" id="eventName" className={styles.inputStyle}></input>
 
-                        <label htmlFor="eventLocalization" className={styles.labelStyle}>Insira a localização do evento</label>
-                        <input type="text" id="eventLocalization" className={styles.inputStyle}></input>
+                            <p className={styles.labelStyle}>Insira a descrição do evento</p>
+                            <textarea id="eventDescription" className={styles.textArea}></textarea>
 
-                        <label htmlFor="date" className={styles.labelStyle}>Insira a data e horário do evento</label>
-                        <DatePicker
-                            selected={selecionedDate}
-                            onChange={(date) => setDate(date)}
-                            showTimeSelect
-                            timeFormat="HH:mm"
-                            timeINtervals={15}
-                            dateFormat="dd/MM/yyyy HH:mm"
-                            locale="pt"
-                            minDate={now}
-                            minTime={sameDate ? now : new Date().setHours(0, 0, 0, 0)}
-                            maxTime={new Date().setHours(23, 59, 59, 999)}
-                            showMonthDropdown
-                            showYearDropdown
-                            dropdownMode="select"
-                            placeholderText="dd/MM/yyyy HH:mm"
-                            className={styles.datepickerInput}
-                        // customInput = {
-                        //     <input className = {styles.dateInput} placeholder = "socorro" />
-                        // }
-                        />
-                        <label htmlFor="price" className={styles.labelStyle}>Insira o valor do ingresso</label>
-                        <input type="text" id="price" className={styles.inputStyle}></input>
+                            <label htmlFor="eventLocalization" className={styles.labelStyle}>Insira a localização do evento</label>
+                            <input type="text" id="eventLocalization" className={styles.inputStyle}></input>
 
-                        <button type="submit" className={styles.buttonStyle} onClick = {handleSubmit}>Adicionar evento</button>
-                    </form>
+                            <label htmlFor="date" className={styles.labelStyle}>Insira a data e horário do evento</label>
+                            <div className={styles.datepickerDiv}>
+                                <DatePicker
+                                    selected={selecionedDate}
+                                    onChange={(date) => setDate(date)}
+                                    showTimeSelect
+                                    timeFormat="HH:mm"
+                                    timeINtervals={15}
+                                    dateFormat="dd/MM/yyyy HH:mm"
+                                    locale="pt"
+                                    minDate={now}
+                                    minTime={sameDate ? now : new Date().setHours(0, 0, 0, 0)}
+                                    maxTime={new Date().setHours(23, 59, 59, 999)}
+                                    showMonthDropdown
+                                    showYearDropdown
+                                    dropdownMode="select"
+                                    placeholderText="dd/MM/yyyy HH:mm"
+                                    className={styles.customDatepicker}
+                                />
+                            </div>
+                            <label htmlFor="price" className={styles.labelStyle}>Insira o valor do ingresso</label>
+                            <input type="text" id="price" className={styles.inputStyle}></input>
+
+                            <button type="submit" className={styles.buttonStyle} onClick={handleSubmit}>Adicionar evento</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </>
